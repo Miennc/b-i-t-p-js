4252517900b8921b21a5ad01630ba069760b3fe6
Mien <2502mien@gmail.com>
2022-03-09 15:40:30 +0700

    add session 2

--- giai_thua.js
+++
@@ -1,9 +0,0 @@
-// tính giai thừa
-function giaiThua(n) {
-    var giaiThua = 1;
-    for (var i = 1; i <= n; i++) {
-        giaiThua = giaiThua * i;
-    }
-    return giaiThua;
-}
-console.log(giaiThua(5));
